679227adcd9d49a989cd749684fa5daa6dcb4926
Ika Khoza <ikaneng.khoza@gmail.com>
2022-07-15 12:04:00 +0100

    removed efile
    file removed

--- server/api.js
+++ server/api.js
@@ -1,35 +1,35 @@
 import { Router } from "express";
-//const { S3Client } = require("@aws-sdk/client-s3");
 const multer = require("multer");
-
-//const multerS3 = require("multer-s3");
+const upload = multer({ dest: "uploads/" });
+const util = require("util");
+const fs = require("fs");
+const unlinkFile = util.promisify(fs.unlink);
 
 const router = Router();
 
-//const s3 = new S3Client();
+const { uploadFile, getFileStream } = require("./s3");
 
-router.get("/", (_, res) => {
-	res.json({ message: "world" });
+router.get("/images/:key", (req, res) => {
+	console.log(req.params);
+	const key = req.params.key;
+	const readStream = getFileStream(key);
+
+	readStream.pipe(res);
 });
 
-const fileStorageEngine = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "./images");
-	},
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + "--" + file.originalname);
-	},
-});
+router.post("/images", upload.single("image"), async (req, res) => {
+	const file = req.file;
+	console.log(file);
 
-const upload = multer({ storage: fileStorageEngine });
+	// apply filter
+	// resize
 
-router.post("/upload", upload.single("image"), (req, res) => {
-	const title = req.body.title;
-	const author = req.body.author;
-
-	console.log(req.file);
-	console.log(title, author);
-	res.send("single file upload success");
+	const result = await uploadFile(file);
+	await unlinkFile(file.path);
+	console.log(result);
+	const description = req.body.description;
+	console.log(description);
+	res.send({ imagePath: `/images/${result.Key}` });
 });
 
 export default router;
