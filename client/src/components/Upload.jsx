21767c869d58f9a7e1996bc28588a0200aa22436
Ika Khoza <ikaneng.khoza@gmail.com>
2022-07-29 15:17:17 +0100

    upload css
    added styles to css

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Navbar from "../components/Navbar.jsx";
 
-import "./Upload.css";
+import "../Upload.css";
 
 function Upload() {
 	const [fileData, setFileData] = useState();
 	const [username, setUserName] = useState();
 	const [description, setDescription] = useState();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	const fileChangeHandler = (e) => {
 		setFileData(e.target.files[0]);
@@ -33,42 +35,49 @@ function Upload() {
 			.catch((err) => {
 				console.log(err.message);
 			});
-      navigate("/");
+		navigate("/home/this/site");
 	};
 
 	return (
-		<div className="container">
-			<form onSubmit={onSubmitHandler}>
-				<label className="upload-select">
-					Select
-					<input className="input-file" type="file" onChange={fileChangeHandler} />
-				</label><br></br>
+		<>
+			<div className="general">
+				<Navbar />
+			</div>
+			<div className="upload">
+				<form onSubmit={onSubmitHandler}>
+					<label className="upload-select">
+						Select
+						<CloudUploadIcon size="large" />
+						<input
+							className="input-file "
+							type="file"
+							onChange={fileChangeHandler}
+						/>
+					</label><br></br>
 
-				<label>
-					username
-					<input
-						type="text"
-						name="description"
-						onChange={(e) => setUserName(e.target.value)}
-					/>
-				</label><br></br>
+					<label>
+						<input
+							type="text"
+							placeholder="username"
+							name="username"
+							onChange={(e) => setUserName(e.target.value)}
+						/>
+					</label>
 
-				<label>
-					Add description and location
-					<input
-						type="text"
-						name="description"
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-				</label><br></br>
+					<label>
+						<textarea placeholder="add location and description"
+							onChange={(e) => setDescription(e.target.value)}
+						></textarea>
+					</label>
+					<br></br>
 
-				<button type="submit" className="btn btn-info">
-					Upload
-				</button>
-			</form>
-		</div>
+					<button type="submit" className="btn btn-dark">
+						Upload
+					</button>
+				</form>
+			</div>
+		</>
 	);
-
 }
 
 export default Upload;
